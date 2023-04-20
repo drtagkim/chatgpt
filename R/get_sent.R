c3e5b9827d405faa294213db9930d3bfe08782de
DRTAG <masan.korea@gmail.com>
2023-04-20 18:09:34 +0900

    Update get_sent.R

--- R/get_sent.R
+++ R/get_sent.R
@@ -1,22 +1,12 @@
 gpt_sent <- function(txt) {
-  p1="For each item do sentiment analysis.
-Range from -1 to 1.
-Show me scores in HTML table at the end."
-  if(length(txt)==1) {
-    txt1=c(txt,"null")
-  } else {
-    txt1=txt
-  }
-  p2=paste(txt1,collapse='\n')
-  prompt=paste0(p1,"\nitems:\n",p2)
-  x = ask_chatgpt(prompt)
-  y = rvest::read_html(x)
-  y = y %>% html_table()
-  if(length(y)>0) {
-    y=y[[1]]
-    if(length(txt)==1) y=y[1,]
-  } else {
-    y = NULL
-  }
+  p0='
+Task: sentiment analysis 
+DO NOT reply anything but the score.
+Give me scores as numbers (decimal=2).
+Score range should be -1(negative) to 1(positive)
+Text:'
+  p1=paste0(p0,txt)
+  x=ask_chatgpt(p1)
+  y=x %>% str_match('[0-1.-]+[0-9]{0,2}') %>% as.numeric()
   y
 }
